05630d958504162aaa36bf86d5e09e55564696e3
Vasily Titov <v.titov@faberlic.com>
2024-10-23 21:58:28 +0400

    86c0t2xz5: Added navigation to header

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MainMenuComponent } from "./components/main-menu/main-menu.component";
 
 @Component({
   selector: 'rcp-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MainMenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
