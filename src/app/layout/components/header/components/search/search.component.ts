e2d4ec37cf253e7596eb211a4f9d7428ed4dbf79
Vasily Titov <v.titov@faberlic.com>
2024-10-24 21:16:53 +0400

    86c0t2y56: Added search and expandable input

---
+++ src/app/layout/components/header/components/search/search.component.ts
@@ -0,0 +1,37 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rcp-search',
+  standalone: true,
+  imports: [],
+  providers: [],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss',
+  animations: [
+    trigger('openClose', [
+      state('closed', style({
+        // transform: 'scaleX(0)',
+        width: '0',
+        opacity: 0,
+        // transformOrigin: 'right'
+      })),
+      state('open', style({
+        // transform: 'scaleX(1)',
+        width: '10rem',
+        opacity: 1,
+        // transformOrigin: 'right'
+      })),
+      transition('open <=> closed', [
+        animate('300ms ease-in-out')
+      ]),
+    ])
+  ]
+})
+export class SearchComponent {
+  isOpen: boolean = false;
+
+  toggle(): void {
+    this.isOpen = !this.isOpen;
+  }
+}
--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MainMenuComponent } from "./components/main-menu/main-menu.component";
+import { SearchComponent } from "./components/search/search.component";
 
 @Component({
   selector: 'rcp-header',
   standalone: true,
-  imports: [RouterLink, MainMenuComponent],
+  imports: [RouterLink, MainMenuComponent, SearchComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
