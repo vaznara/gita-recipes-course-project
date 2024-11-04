91c52f77808bd89867468c7e845ad63114c499f0
Vasily Titov <v.titov@faberlic.com>
2024-11-04 15:17:39 +0400

    86c0w0gvn: Added recipe view page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RecipeViewComponent } from './pages/recipe-view/recipe-view.component';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'recipe', component: RecipeViewComponent }
+];
