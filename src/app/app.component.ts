ee6f4ba405bbff177fe4979760bb012e7dabcc5f
Vasily Titov <v.titov@faberlic.com>
2024-11-06 22:18:34 +0400

    86c0y2rwm: Added global loader (spinner) service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { AuthService } from './shared/services';
+import { LoaderService } from './shared/services/loader.service';
+import { AsyncPipe, DOCUMENT } from '@angular/common';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'rcp-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent],
+  imports: [RouterOutlet, LayoutComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
-  constructor(private authService: AuthService) { }
+  ngUnsubscribe$: Subject<void> = new Subject();
+
+  isLoading: boolean = false;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private authService: AuthService,
+    private renderer: Renderer2,
+    public loaderService: LoaderService
+  ) { }
+
+  ngOnInit(): void {
+
+    this.authService.checkAuthState();
+
+    this.loaderService.isLoading$.pipe(takeUntil(this.ngUnsubscribe$))
+      .subscribe(state => {
+        state
+          ? this.renderer.addClass(this.document.body, 'noscroll')
+          : this.renderer.removeClass(this.document.body, 'noscroll');
+
+        this.isLoading = state;
+      })
+
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe$.next();
+    this.ngUnsubscribe$.complete();
+  }
 }
