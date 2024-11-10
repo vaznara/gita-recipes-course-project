bcd714449acc08d01ae0d4bb1468fe65144fb096
Vasily Titov <v.titov@faberlic.com>
2024-11-10 16:34:19 +0400

    86c0z6nce: Added user access directive

--- src/app/shared/components/recipes-listing/recipes-listing.component.ts
+++ src/app/shared/components/recipes-listing/recipes-listing.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { ICategory, IRecipeResponse } from '../../interfaces/interface';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { UserAccessDirective } from '../../directives/user-access.directive';
 
 @Component({
   selector: 'rcp-recipes-listing',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, UserAccessDirective],
   templateUrl: './recipes-listing.component.html',
   styleUrl: './recipes-listing.component.scss'
 })
@@ -13,4 +14,10 @@ export class RecipesListingComponent {
 
   @Input() category: ICategory | null = null;
   @Input() recipes: IRecipeResponse[] = [];
+
+  constructor(private router: Router) { }
+
+  onEdit(key: string): void {
+    this.router.navigate([`/user/recipe/edit/${key}`]);
+  }
 }
---
+++ src/app/shared/directives/user-access.directive.ts
@@ -0,0 +1,35 @@
+import { AfterViewInit, Directive, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../services';
+import { Subject, takeUntil } from 'rxjs';
+
+@Directive({
+  selector: '[rcpUserAccess]',
+  standalone: true
+})
+export class UserAccessDirective implements AfterViewInit, OnDestroy {
+
+  private readonly ngUnsubscribe$: Subject<void> = new Subject();
+
+  @Input('rcpUserAccess') contentOwnerUid?: string;
+
+  constructor(
+    private elementRef: ElementRef,
+    private authService: AuthService
+  ) { }
+
+  ngAfterViewInit(): void {
+    this.authService.currentUser$.pipe(takeUntil(this.ngUnsubscribe$))
+      .subscribe(user => {
+        console.log(this.contentOwnerUid)
+        if (!this.contentOwnerUid || user?.uid !== this.contentOwnerUid) {
+          this.elementRef.nativeElement.remove();
+        }
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe$.next();
+    this.ngUnsubscribe$.complete();
+  }
+
+}
