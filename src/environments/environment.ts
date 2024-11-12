d671b00778d4d6fdb02653e65d3311e0ebf53cd2
Vasily Titov <v.titov@faberlic.com>
2024-11-12 21:53:44 +0400

    86c103fd1: Updated environment variables config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,2 +1,16 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const environment = {
+  production: true,
+  firebase: {
+    projectId: process.env["PROJECT_ID"] || '',
+    appId: process.env["APP_ID"] || '',
+    storageBucket: process.env["STORAGE_BUCKET"] || '',
+    apiKey: process.env["API_KEY"] || '',
+    authDomain: process.env["AUTH_DOMAIN"] || '',
+    messagingSenderId: process.env["MESSAGING_SENDER_ID"] || ''
+  },
+  dbPath: process.env["REST_API_PATH"] || '',
+  maxFileSize: process.env["MAX_FILE_SIZE"] || ''
 }
