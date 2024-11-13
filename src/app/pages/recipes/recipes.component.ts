714685c66953210e2c53e4bf4058171869eaf8ba
Vasily Titov <v.titov@faberlic.com>
2024-11-14 00:03:39 +0400

    86c10k0x6: Added pagination

--- src/app/pages/recipes/recipes.component.ts
+++ src/app/pages/recipes/recipes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ICategory, IRecipeResponse } from '../../shared/interfaces/interface';
-import { concatMap, of, Subject, takeUntil } from 'rxjs';
+import { concatMap, of, Subject, take, takeUntil } from 'rxjs';
 import { CategoryService, RecipeService } from '../../shared/services';
 import { RecipesListingComponent } from '../../shared/components/recipes-listing/recipes-listing.component';
 import { Title } from '@angular/platform-browser';
@@ -17,9 +17,13 @@ export class RecipesComponent implements OnInit, OnDestroy {
 
   ngUnsubscribe$: Subject<void> = new Subject();
 
+  private readonly pageSize: number = 20;
+
   category: ICategory | null = null;
   categoryKey: string | null = null;
   recipes: IRecipeResponse[] = [];
+
+  canLoadMore = true;
 
   constructor(private route: ActivatedRoute,
     private recipeService: RecipeService,
@@ -39,11 +43,25 @@ export class RecipesComponent implements OnInit, OnDestroy {
       concatMap((category) => {
         this.category = category;
         this.title.setTitle(`Recipes from ${this.category?.name} category`);
-        return this.categoryKey ? this.recipeService.getRecipesByCategory(this.categoryKey) : this.recipeService.getRecipes();
+        return this.categoryKey ? this.recipeService.getRecipesByCategory(this.categoryKey) : this.recipeService.getRecipes(this.pageSize);
       })
     )
       .subscribe(res => {
+        if (res.length !== this.pageSize) {
+          this.canLoadMore = false;
+        }
         this.recipes = res;
+      })
+  }
+
+  loadRecipes(): void {
+    this.recipeService.getRecipes(this.pageSize, this.recipes[this.recipes.length - 1].key)
+      .pipe(take(1))
+      .subscribe((res) => {
+        if (res.length !== this.pageSize) {
+          this.canLoadMore = false;
+        }
+        this.recipes = [...this.recipes, ...res];
       })
   }
 
